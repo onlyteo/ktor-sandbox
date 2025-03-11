d99b7964ffd5816eae2327ebaf28176befb44bfd
Thomas <onlyteo@live.com>
2025-03-11 17:26:32 +0100

    Moved apps into dedicated folder

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -17,31 +17,31 @@ dependencyResolutionManagement {
 
 rootProject.name = "ktor-sandbox"
 
-include(":ktor-rest-api:frontend")
-include(":ktor-rest-api:frontend-api")
-include(":ktor-rest-api:backend")
+include(":apps:ktor-rest-api:frontend")
+include(":apps:ktor-rest-api:frontend-api")
+include(":apps:ktor-rest-api:backend")
 
-include(":ktor-crud-exposed-jdbc:frontend")
-include(":ktor-crud-exposed-jdbc:backend")
+include(":apps:ktor-crud-exposed-jdbc:frontend")
+include(":apps:ktor-crud-exposed-jdbc:backend")
 
-include(":ktor-crud-exposed-dao:frontend")
-include(":ktor-crud-exposed-dao:backend")
+include(":apps:ktor-crud-exposed-dao:frontend")
+include(":apps:ktor-crud-exposed-dao:backend")
 
-include(":ktor-crud-ktorm:frontend")
-include(":ktor-crud-ktorm:backend")
+include(":apps:ktor-crud-ktorm:frontend")
+include(":apps:ktor-crud-ktorm:backend")
 
-include(":ktor-crud-jooq:frontend")
-include(":ktor-crud-jooq:backend")
+include(":apps:ktor-crud-jooq:frontend")
+include(":apps:ktor-crud-jooq:backend")
 
-include(":ktor-oauth2-token-relay:frontend")
-include(":ktor-oauth2-token-relay:frontend-api")
-include(":ktor-oauth2-token-relay:backend")
-include(":ktor-oauth2-token-relay:authorization-server")
+include(":apps:ktor-oauth2-token-relay:frontend")
+include(":apps:ktor-oauth2-token-relay:frontend-api")
+include(":apps:ktor-oauth2-token-relay:backend")
+include(":apps:ktor-oauth2-token-relay:authorization-server")
 
-include(":ktor-cqrs-kafka:frontend")
-include(":ktor-cqrs-kafka:frontend-api")
-include(":ktor-cqrs-kafka:backend")
+include(":apps:ktor-cqrs-kafka:frontend")
+include(":apps:ktor-cqrs-kafka:frontend-api")
+include(":apps:ktor-cqrs-kafka:backend")
 
-include(":ktor-otel-observability:frontend")
-include(":ktor-otel-observability:frontend-api")
-include(":ktor-otel-observability:backend")
+include(":apps:ktor-otel-observability:frontend")
+include(":apps:ktor-otel-observability:frontend-api")
+include(":apps:ktor-otel-observability:backend")
